8acd9b7a9a205ffbc59956afc1216e37944ca194
antkeyes <keyes.anthony@gmail.com>
2022-01-14 13:15:14 -0500

    completed isLeapYear function

--- mini-calendar.c
+++ mini-calendar.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+bool isLeapYear(int year) {
+
+    if (year % 400 == 0) {
+        return true;
+    } else if (year % 4 == 0 && year % 100 != 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
 int main() {
 
+    int year;
+    printf("Please enter a year between 1800 and 10000: ");
+    scanf("%d", &year);
 
-
-}
-
-void isLeapYear(void) {
+    int conclusion = isLeapYear(year);
     
+
+    printf("Is the year %d a leap year?: %d\n", year, conclusion);
+
+
+
 }
+
